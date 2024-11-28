cad20474cf70d2c2e5a4a841818891bfc4dd7b1e
RogersSC <ssras@gmail.com>
2024-11-27 23:26:51 -0400

    add ssl

--- src/database/database.js
+++ src/database/database.js
@@ -9,6 +9,14 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST, 
         dialect: process.env.DB_DIALECT,
         logging: console.log,
+
+        //habilitar en modo productivo y comentar en modo local
+        dialectOptions:{
+            ssl:{
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
 });
 
 export default sequelize;
